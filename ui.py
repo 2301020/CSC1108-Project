da1d2bf2466f220ab39216534ce96ae594811a55
Xue Bin <xuebinsng@gmail.com>
2024-03-27 18:58:56 +0800

    Adding algor selection and displaying output

--- ui.py
+++ ui.py
@@ -95,33 +95,36 @@ class App(customtkinter.CTk):
         
         self.radio_frame = customtkinter.CTkFrame(self.frame_right, corner_radius=0)
         self.radio_frame.grid(column=0, row=6, sticky="EW", padx=5, pady=5)
+        self.radio_frame.columnconfigure(0, weight=2)
+        
+        self.options_label = customtkinter.CTkLabel(self.radio_frame, text="Options", font=("Helvetica", 15))
+        self.options_label.grid(column=0, row=0, sticky="EW", padx=5, pady=5)
         
         self.selected_radiobox = customtkinter.StringVar()
         r1 = customtkinter.CTkRadioButton(self.radio_frame, text="Shortest Path", variable=self.selected_radiobox, value="shortest_path")
         r2 = customtkinter.CTkRadioButton(self.radio_frame, text="Cheapest Path", variable=self.selected_radiobox, value="cheapest_path")
         r3 = customtkinter.CTkRadioButton(self.radio_frame, text="Fastest Path", variable=self.selected_radiobox, value="fastest_path")
-        r1.grid(column=0, row=0, sticky="EW", padx=5, pady=5)
-        r2.grid(column=0, row=1, sticky="EW", padx=5, pady=5)
-        r3.grid(column=0, row=2, sticky="EW", padx=5, pady=5)
-
+        r1.grid(column=0, row=1, sticky="EW", padx=5, pady=5)
+        r2.grid(column=0, row=2, sticky="EW", padx=5, pady=5)
+        r3.grid(column=0, row=3, sticky="EW", padx=5, pady=5)
+        
+        self.algor_label = customtkinter.CTkLabel(self.radio_frame, text="Algorithm selection", font=("Helvetica", 15))
+        self.algor_label.grid(column=0, row=4, sticky="EW", padx=5, pady=5)
+        
+        self.algorthim_selection = customtkinter.StringVar()
+        self.algor_dropDownList = customtkinter.CTkComboBox(self.radio_frame, corner_radius=0, fg_color=None, values=["dijkstra", "astar", "Bellman-Ford"], variable=self.algorthim_selection, cursor="hand2", state="readonly" )
+        self.algorthim_selection.set("Dijkstra")
+        self.algor_dropDownList.grid(column=0, row=5, sticky="EW", padx=5, pady=10)
+        
         self.search_button = customtkinter.CTkButton(self.frame_right, text="Search", command=self.search)
         self.search_button.grid(column=0, row=7, sticky="EW", padx=5, pady=5)
 
         self.airport_list = []
-        self.airport_route = None
+        self.airport_route = []
 
-        source = self.Location("Singapore", 1.3558572118659549, 103.98638538648154)
-        destination = self.Location("Kuala Lumpur", 3.1578589977287805, 101.70339753766487)
-        
-        self.displayFlightResults(source, destination)
     
     def getAirports(self):
         self.airport_list = sorted([airport.name for airport in self.flight_pathing.idToAirportMap.values()])    # temp class        
-    class Location:
-        def __init__(self, name, latitude, longitude):
-            self.name = name
-            self.latitude = latitude
-            self.longitude = longitude    
     
     def roundTripToggle(self):
         self.round_trip_switch_state.set("Round trip" if self.round_trip_switch_state.get() == "Single trip" else "Single trip")
@@ -148,29 +151,58 @@ class App(customtkinter.CTk):
         # shortest_path = self.flight_pathing.getShortestPath(self.source, self.destination)
         # print(shortest_path)
         
-        sourceLocation = self.map_widget.set_address(source, marker=True)
-        destinationLocation = self.map_widget.set_address(destination, marker=True)
-        self.map_widget.set_path([sourceLocation.position, destinationLocation.position], width=3, color="blue")
-        self.map_widget.set_position(sourceLocation.position, zoom=5)
+        self.airport_route = self.flight_pathing.getShortestPathWithObjects(source, destination, self.get_radiobutton_value()) #, self.algorthim_selection.get())
+        print(self.airport_route)
+        self.displayFlightResults(self.airport_route)
+        # sourceLocation = self.map_widget.set_address(source, marker=True)
+        # destinationLocation = self.map_widget.set_address(destination, marker=True)
+        # self.map_widget.set_path([sourceLocation.position, destinationLocation.position], width=3, color="blue")
+        # self.map_widget.set_position(sourceLocation.position, zoom=5, deg_y=0, deg_x=0)
 
-    def plotPath(self, path):
-        next_path = None
-        for location in path:
-            marker_1 = self.map_widget.set_marker(location.longtitude, location.latitude, text=location.name)
-            if next_path is not None:
-                marker_2 = self.map_widget.set_marker(next_path.longtitude, next_path.latitude, text=next_path.name)
-                self.map_widget.set_path([marker_1.position, marker_2.position], width=3, color="red")
-            next_path = location            
+    def get_radiobutton_value(self)->FlightMapRouting.SearchParameter:
+        search_param = SearchParam()
+        match(self.selected_radiobox.get()):
+            case "shortest_path":
+                search_param.cost = 0.1
+                search_param.time = 0.9
+            case "cheapest_path":
+                search_param.cost = 0.9
+                search_param.time = 0.1
+            case "fastest_path":
+                search_param.cost = 0.1
+                search_param.time = 0.9
+        return search_param
 
-    def displayFlightResults(self, source, destination):
+    def plotPath(self, airport: FlightMapRouting.Airport, previous_airport: FlightMapRouting.Airport):
+        marker_1 = self.map_widget.set_marker(airport.latitude, airport.longitude, text=airport.name)
+        if previous_airport is not None:
+            marker_2 = self.map_widget.set_marker(previous_airport.longtitude, previous_airport.latitude, text=previous_airport.name)
+            self.map_widget.set_path([marker_1.position, marker_2.position], width=3, color="red")
+
+    def displayFlightResults(self, airportList: list[FlightMapRouting.Airport]):
         self.flight_info = customtkinter.CTkLabel(self.frame_bottom, text="Flight Information")
-        self.flight_info.grid(column=0, row=6, sticky="EW", padx=5, pady=5)
-
-        self.flight_starting_country = customtkinter.CTkLabel(self.frame_bottom, text=f"Starting Country: {source.name}")
-        self.flight_starting_country.grid(column=0, row=7, sticky="EW", padx=5, pady=5)
+        self.flight_info.grid(column=0, row=0, sticky="EW", padx=5, pady=5)
+        self.starting_airport = customtkinter.CTkLabel(self.frame_bottom, text="Starting Airport")
+        self.starting_airport.grid(column=0, row=1, sticky="EW", padx=5, pady=5)
         
-    def createAirportFrame(self, airport)->customtkinter.CTkFrame:
-        airport_frame = customtkinter.CTkFrame(self, corner_radius=0, relief="solid", borderwidth=1)
+
+        self.flight_info_frame = customtkinter.CTkScrollableFrame(self.frame_bottom, corner_radius=0)
+        self.flight_info_frame.grid(column=0, row=2, sticky="EW", padx=5, pady=5)
+        
+        previous_airport = None
+        for index,airport in enumerate(airportList):
+            self.plotPath(airport, previous_airport)
+            self.stop_number = customtkinter.CTkLabel(self.flight_info_frame, text=f"Stop {index+1}")
+            self.stop_number.grid(column=0, row=index, sticky="EW", padx=5, pady=5)
+            
+            airport_frame = customtkinter.CTkFrame(self.flight_info_frame, corner_radius=0, relief="solid", borderwidth=1)
+            self.createAirportFrame(airport_frame,airport)
+            airport_frame.grid(column=1, row=index, sticky="EW", padx=5, pady=5)
+            
+            previous_airport = airport
+                
+        
+    def createAirportFrame(airport_frame: customtkinter.CTkFrame, airport: FlightMapRouting.Airport)->customtkinter.CTkFrame:
         airport_frame.grid_columnconfigure(0, weight=1)
         airport_frame.grid_columnconfigure(1, weight=1)
         airport_frame.grid_columnconfigure(2, weight=1)
@@ -189,7 +221,8 @@ class App(customtkinter.CTk):
         
         airport_ICAO = customtkinter.CTkLabel(airport_frame, text=f"ICAO: {airport.ICAO}")
         airport_ICAO.grid(column=0, row=4, sticky="EW", padx=5, pady=5)
-                
+        
+                        
     def on_closing(self):
         self.destroy()
         exit(0)
